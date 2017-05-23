4aefe93d2ef06b58cdcb0f1a66fa83d4125f33d4
Martin-Kotrulev <martin.kotrulev@gmail.com>
2017-05-23 22:09:55 +0300

    added adding and searching

--- Shop_Stop/handlers/home.js
+++ Shop_Stop/handlers/home.js
@@ -1,12 +1,15 @@
 const url = require('url')
 const fs = require('fs')
 const path = require('path')
+const database = require('../config/database')
+const qs = require('querystring')
 
 module.exports = (req, res) => {
   req.pathname = req.pathname || url.parse(req.url).pathname
 
   if (req.pathname === '/' && req.method === 'GET') {
     let filePath = path.normalize(path.join(__dirname, '../views/home/index.html'))
+    let queryData = qs.parse(url.parse(req.url).query)
 
     fs.readFile(filePath, (err, data) => {
       if (err) {
@@ -23,7 +26,29 @@ module.exports = (req, res) => {
         'Content-Type': 'text/html'
       })
 
-      res.write(data)
+      let products = database.products.getAll()
+
+      if (queryData.query) {
+        console.log(queryData.query)
+        products = products.filter((p) => console.log(p.name.toLowerCase().indexOf(queryData.query)))
+      }
+
+      let content = ''
+      for (let product of products) {
+        content +=
+         `<div class="product-card">
+          <img class="product-img" src="${product.image}">
+          <h2>${product.name}</h2>
+          <p>${product.description}</p>
+        </div>`
+      }
+
+      if (!content && queryData.query) {
+        content = `No results for search '${queryData.query}'`
+      }
+
+      let html = data.toString().replace('{content}', content)
+      res.write(html)
       res.end()
     })
   } else {
--- Shop_Stop/handlers/index.js
+++ Shop_Stop/handlers/index.js
@@ -1,4 +1,5 @@
 const homeHandler = require('./home')
 const filesHandler = require('./static-files')
+const productsHandler = require('./products.js')
 
-module.exports = [homeHandler, filesHandler]
+module.exports = [homeHandler, filesHandler, productsHandler]
