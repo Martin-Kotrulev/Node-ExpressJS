769670cba8a7aefacc33fe97a397506038de8725
Martin-Kotrulev <martin.kotrulev@gmail.com>
2017-06-12 23:34:57 +0300

    added 'User' schema model

---
+++ ArchAndAuth/server/data/User.js
@@ -0,0 +1,43 @@
+const mongoose = require('mongoose')
+const encryption = require('../encryption')
+const REQUIRED_VALIDATION_MESSAGE = '{PATH} is required'
+
+let userSchema = new mongoose.Schema({
+  username: { type: String, required: REQUIRED_VALIDATION_MESSAGE, unique: true },
+  firstName: { type: String, required: REQUIRED_VALIDATION_MESSAGE },
+  lastName: { type: String, required: REQUIRED_VALIDATION_MESSAGE },
+  salt: String,
+  hashedPass: String,
+  roles: [String]
+})
+
+userSchema.method({
+  authenticate: (password) => {
+    if (encryption.generateHashedPassword(this.salt, password) === this.hashedPass) {
+      return true
+    } else {
+      return false
+    }
+  }
+})
+
+let User = mongoose.model('User', userSchema)
+
+module.exports.seedAdminUser = () => {
+  User.find({}).then((users) => {
+    if (users.length > 0) {
+      return
+    }
+
+    let salt = encryption.generateSalt()
+    let hashedPass = encryption.generateHashedPassword(salt, '123456')
+    User.create({
+      username: 'joe.black',
+      firstName: 'Joe',
+      lastName: 'Black',
+      salt: salt,
+      hashedPass: hashedPass,
+      roles: ['Admin']
+    })
+  })
+}
