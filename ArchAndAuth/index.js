7cf04286d62427e27762ad3a42bdba7864c6809e
Martin-Kotrulev <martin.kotrulev@gmail.com>
2017-06-12 20:37:38 +0300

    added handlebars and layouts

--- ArchAndAuth/index.js
+++ ArchAndAuth/index.js
@@ -1,9 +1,20 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const handlebars = require('express-handlebars')
 
 mongoose.Promise = global.Promise
 
+let env = process.env.NODE_ENV || "development"
+
+let port = process.env.PORT || 1337
+
 let app = express()
+
+app.engine('handlebars', handlebars({
+  defaultLayout: 'main'
+}))
+
+app.set("view engine", "hbs")
 
 app.get('/', (req, res) => {
   console.log('Express working...')
